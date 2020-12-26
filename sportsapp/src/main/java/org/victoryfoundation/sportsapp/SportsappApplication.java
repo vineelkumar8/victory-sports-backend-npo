aeaa6c6089ad6b20387a52be0cb889e322e895e4
svusa <svusa@paypal.com>
2020-12-26 10:30:06 +0530

    Creating new app

---
+++ sportsapp/src/main/java/org/victoryfoundation/sportsapp/SportsappApplication.java
@@ -0,0 +1,13 @@
+package org.victoryfoundation.sportsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsappApplication.class, args);
+	}
+
+}
---
+++ sportsapp/src/test/java/org/victoryfoundation/sportsapp/SportsappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.victoryfoundation.sportsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
